550cd4a940a345f5166560085d1eda914bba45ff
yunadada <ynson0216@naver.com>
2025-10-27 16:32:09 +0900

    chore(app): integrate core modules into main App entry

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import validateInput from "./validateInput.js";
+import createCars from "./createCars.js";
+import runRace from "./runRace.js";
+import findWinners from "./findWinners.js";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const carNames = await MissionUtils.Console.readLineAsync(
+        "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+      );
+      const attemptCount = await MissionUtils.Console.readLineAsync(
+        "시도할 횟수는 몇 회인가요?\n"
+      );
+
+      const parsedNames = validateInput(carNames, attemptCount);
+      const cars = createCars(parsedNames);
+
+      MissionUtils.Console.print("\n실행 결과");
+      for (let i = 0; i < attemptCount; i++) {
+        runRace(cars);
+        MissionUtils.Console.print("");
+      }
+
+      const winners = findWinners(cars);
+      MissionUtils.Console.print(`최종 우승자 : ${winners}`);
+    } catch (error) {
+      throw error;
+    }
+  }
 }
 
 export default App;
